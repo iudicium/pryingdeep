a9a81616a0c8eacf16bc4e70d18f743e905ac6f0
Issekai <issekai666@proton.me>
2024-01-14 18:14:47 +0700

    Delete some test files

--- tests/querybuilder/data/associations.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM web_pages
-INNER JOIN cryptos ON web_pages.id = cryptos.web_page_id
-INNER JOIN emails ON web_pages.id = emails.web_page_id
-INNER JOIN phone_numbers ON web_pages.id = phone_numbers.web_page_id
-INNER JOIN wordpress ON web_pages.id = wordpress.web_page_id
